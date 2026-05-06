a05b0ef0cb2d32ad146d810af4c581aa2ca47991
skjaberbhy1 <skjaber01@gmail.com>
2026-05-07 01:09:07 +0600

    production3

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -13,3 +13,27 @@ resource "aws_subnet" "public_2" {
   cidr_block        = "10.0.2.0/24"
   availability_zone = "ap-southeast-1b"
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public.id
+}
+
